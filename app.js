58b58e6c326a3022bcf3e023881051b0cba2d14d
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-02-11 20:08:48 +0100

    feat: add menu to app

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-const argv = require('./config/yargs').argv;
-const colors = require('colors');
-const porHacer = require('./por-hacer/por-hacer');
+const argv = require("./config/yargs").argv;
+const colors = require("colors");
+const { mostrarMenu, pausa } = require("./helpers/mensajes");
 
-let comando = argv._[0];
+const main = async () => {
+  let opt = "";
+  do {
+    opt = await mostrarMenu();
+    console.log({ opt });
+    if (opt !== "0") await pausa();
+  } while (opt !== "0");
+};
 
-switch (comando) {
-    case 'crear':
-        let tarea = porHacer.crear(argv.descripcion);
-        console.log(tarea);
-        break;
-    case 'listar':
-        let listado = porHacer.getlistado(argv.completado);
-        for (let tarea of listado) {
-            console.log('=====Por Hacer====='.green);
-            console.log(tarea.descripcion);
-            console.log('Estado: ', tarea.completado);
-            console.log('===================\n'.green);
-        }
-        break;
-    case 'actualizar':
-        let actualizado = porHacer.actualizar(argv.descripcion, argv.completado);
-        console.log(actualizado);
-        break;
+main();
+/* switch (comando) {
+  case "crear":
+    let tarea = porHacer.crear(argv.descripcion);
+    console.log(tarea);
+    break;
+  case "listar":
+    let listado = porHacer.getlistado(argv.completado);
+    for (let tarea of listado) {
+      console.log("=====Por Hacer=====".green);
+      console.log(tarea.descripcion);
+      console.log("Estado: ", tarea.completado);
+      console.log("===================\n".green);
+    }
+    break;
+  case "actualizar":
+    let actualizado = porHacer.actualizar(argv.descripcion, argv.completado);
+    console.log(actualizado);
+    break;
 
-    case 'borrar':
-        let borrado = porHacer.borrar(argv.descripcion);
-        console.log(borrado);
-        break;
-    default:
-        console.log('El comando no es reconocido');
-        break;
-}
+  case "borrar":
+    let borrado = porHacer.borrar(argv.descripcion);
+    console.log(borrado);
+    break;
+  default:
+    console.log("El comando no es reconocido");
+    break;
+} */
